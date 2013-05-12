4013a68eb177deb2494af62406cd1cb96ad57504
Jon Henderson <j.henderson@capstonedigital.com>
2013-05-12 00:59:26 -0500

    simplifies autoloader

--- src/Chevron/autoload/autoload.php
+++ src/Chevron/autoload/autoload.php
@@ -1,9 +1,5 @@
 <?php
 
-error_reporting(E_ALL ^ E_NOTICE);
-
-define('IS_CLI', 0 === stripos(php_sapi_name(), "cli"));
-
 /**
  * Define an autoloader where the application will look for a class file within
  * the new library folder
@@ -12,25 +8,9 @@ define('IS_CLI', 0 === stripos(php_sapi_name(), "cli"));
  * @param $request $class The name of the file to be auto loaded
  */
 spl_autoload_register(function ( $request ) {
-	$sep    = DIRECTORY_SEPARATOR;
-	$parts  = explode("\\", $request);
-
-	$final = "";
-	while($part = array_pop($parts)){
-		$final = "{$sep}{$part}{$final}";
-	}
-
-	$final = ltrim($final, "/");
-	$file = __DIR__ . "/../../{$final}.php";
-
-	if( file_exists( $file ) ){
-		include_once( $file );
+	$file = str_replace("\\", DIRECTORY_SEPARATOR, $request);
+	$path = dirname(dirname(__DIR__));
+	if( file_exists( "{$path}/{$file}.php" ) ){
+		include_once( "{$path}/{$file}.php" );
 	}
 });
-
-include_once( __DIR__ . "/../WeightsAndMeasures/WeightsAndMeasures.php");
-include_once( __DIR__ . "/../Misc/debug_functions.php");
-
-set_exception_handler(array('\Chevron\Errors\ExceptionHandler','handle'));
-
-set_error_handler(array('\Chevron\Errors\ErrorHandler','handle'));
