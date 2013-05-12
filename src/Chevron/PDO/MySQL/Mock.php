13088af2e0543fa745dac2faf41c9c2311f08bf3
Jon Henderson <j.henderson@capstonedigital.com>
2013-05-12 14:21:21 -0500

    PDO\MySQL\Mock is now a FIFO stack for testing funcs with 2+ db calls

--- src/Chevron/PDO/MySQL/Mock.php
+++ src/Chevron/PDO/MySQL/Mock.php
@@ -3,72 +3,84 @@
 namespace Chevron\PDO\MySQL;
 /**
  * This class implements the WrapperInterface but every method returns
- * whatever data you've stored in $next (via next()). This should allow
- * for the testing of DB dependent functionality by eliminating a live
- * DB.
+ * whatever data you've stored in $next (via next()) in a FIFO way.
+ * This should allow for the testing of DB dependent functionality by
+ * eliminating a live DB.
  */
 class Mock implements WrapperInterface {
 
-	protected $next;
+	protected $next = array();
 
+	/**
+	 * Operate a FIFO stack of return values
+	 * @param mixed $next The value to return
+	 * @return mixed
+	 */
 	function next($next){
-		$this->next = $next;
+		$this->next[] = $next;
+	}
+	/**
+	 * Enforce a FIFO stack
+	 * @return mixed
+	 */
+	function shift(){
+		return array_shift($this->next);
 	}
 
 	function put($table, array $map, array $where = array()){
-		return $this->next;
+		return $this->shift();
 	}
 
 	function insert($table, array $map){
-		return $this->next;
+		return $this->shift();
 	}
 
 	function update($table, array $map, array $where = array()){
-		return $this->next;
+		return $this->shift();
 	}
 
 	function replace($table, array $map){
-		return $this->next;
+		return $this->shift();
 	}
 
 	function on_duplicate_key($table, array $map, array $where){
-		return $this->next;
+		return $this->shift();
 	}
 
 	function multi_insert($table, array $map){
-		return $this->next;
+		return $this->shift();
 	}
 
 	function multi_replace($table, array $map){
-		return $this->next;
+		return $this->shift();
 	}
 
 	function exe($query, array $map = array(), $in = false){
-		return $this->next;
+		return $this->shift();
 	}
 
 	function assoc($query, array $map = array(), $in = false){
-		return $this->next;
+		return $this->shift();
 	}
 
 	function row($query, array $map = array(), $in = false){
-		return $this->next;
+		return $this->shift();
 	}
 
 	function scalar($query, array $map = array(), $in = false){
-		return $this->next;
+		return $this->shift();
 	}
 
 	function scalars($query, array $map = array(), $in = false){
-		return $this->next;
+		return $this->shift();
 	}
 
 	function keypair($query, array $map = array(), $in = false){
-		return $this->next;
+		return $this->shift();
 	}
 
 	function __call($name, $args){
-		return $this->next;
+		return $this->shift();
 	}
 
 	function __construct($dsn, $username, $password){}
--- test/PDO/MySQL/MockTest.php
+++ test/PDO/MySQL/MockTest.php
@@ -22,6 +22,30 @@ class MockTest extends PHPUnit_Framework_TestCase {
 		$mock->next($expected);
 
 		$value = $next->getValue($mock);
+		$expected = array($expected);
+
+		$this->assertEquals($expected, $value, "Mock::next failed to set the next property");
+
+	}
+
+	/**
+	 * @depends test_implements
+	 */
+	public function test_next_double_func(){
+		$mock = new \Chevron\PDO\MySQL\Mock("", "", "");
+
+		$reflection = new \ReflectionClass($mock);
+		$next = $reflection->getProperty("next");
+		$next->setAccessible(true);
+
+		$expected1 = array(true, "string", null, 123);
+		$expected2 = array(false, "str", function(){ return 123; });
+
+		$mock->next($expected1);
+		$mock->next($expected2);
+
+		$value = $next->getValue($mock);
+		$expected = array($expected1, $expected2);
 
 		$this->assertEquals($expected, $value, "Mock::next failed to set the next property");
 
@@ -56,6 +80,26 @@ class MockTest extends PHPUnit_Framework_TestCase {
 		$value = $mock->insert("", array());
 
 		$this->assertEquals($expected, $value, "Mock::insert failed to return the next property");
+
+	}
+
+	/**
+	 * @depends test_implements
+	 */
+	public function test_return_next_multi(){
+		$mock = new \Chevron\PDO\MySQL\Mock("", "", "");
+
+		$expected1 = array(true, "string", null, 123);
+		$expected2 = array(false, "str", function(){ return 123; });
+
+		$mock->next($expected1);
+		$mock->next($expected2);
+
+		$value1 = $mock->insert("", array());
+		$value2 = $mock->insert("", array());
+
+		$this->assertEquals($expected1, $value1, "Mock::insert failed to return the next property");
+		$this->assertEquals($expected2, $value2, "Mock::insert failed to return the next property");
 
 	}
 
